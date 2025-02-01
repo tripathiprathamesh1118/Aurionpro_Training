dd16366cdbb8d759fe274e8d66e999e8798ebad3
tripathiprathamesh1118 <tripathiprathamesh1118@gmail.com>
2025-02-01 22:25:24 +0530

    Update FindSubstring_refactor.java

--- FindSubstring_refactor.java
+++ FindSubstring_refactor.java
@@ -3,21 +3,21 @@ package com.java;
 import java.util.Scanner;
 
 public class FindSubstring_refactor {
-
+	//Creates function
 	public static boolean isSubstring(String mainString, String subString) {
 		return mainString.contains(subString);
 	}
 
 	public static void main(String[] args) {
 		Scanner userinput = new Scanner(System.in);
-
+		//takes input from user for main string
 		System.out.println("\nEnter any main string: ");
 		String mainString = userinput.nextLine();
-
+		//takes input from user for substring
 		System.out.println("\nEnter any sub string: ");
 		String subString = userinput.nextLine();
 
-		// Call isSubstring function
+		// Calls function
 		if (isSubstring(mainString, subString)) {
 			System.out.println("Substring found: " + subString);
 		} else {
