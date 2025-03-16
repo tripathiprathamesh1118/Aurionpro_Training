1b4253f0761d986ee7a232c44b296b600dace03b
tripathiprathamesh1118 <tripathiprathamesh1118@gmail.com>
2025-03-16 11:02:50 +0530

    Update ResultAnalzer.java

--- TicTacToeFacade/src/com/aurionpro/model/ResultAnalzer.java
+++ TicTacToeFacade/src/com/aurionpro/model/ResultAnalzer.java
@@ -1,35 +1,34 @@
 package com.aurionpro.model;
 
-public class ResultAnalzer {
-	private Board board;
+public class ResultAnalyzer {
+    private Board board;
 
-	public ResultAnalzer(Board board) {
+    public ResultAnalyzer(Board board) {
         this.board = board;
     }
 
-	public String analyzeResult() {
-		if (checkWin(MarkType.X))
-			return "X Wins!";
-		if (checkWin(MarkType.O))
-			return "O Wins!";
-		if (board.isBoardFull())
-			return "It's a Draw!";
-		return "Game in Progress...";
-	}
+    public String analyzeResult() {
+        if (checkWin(MarkType.X))
+            return "X Wins!";
+        if (checkWin(MarkType.O))
+            return "O Wins!";
+        if (board.isBoardFull())
+            return "It's a Draw!";
+        return "Game in Progress...";
+    }
 
-	private boolean checkWin(MarkType mark) {
-		int[][] winPatterns = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, // Rows
-				{ 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, // Columns
-				{ 0, 4, 8 }, { 2, 4, 6 } // Diagonals
-		};
+    private boolean checkWin(MarkType mark) {
+        int[][] winPatterns = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 },
+                { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 },
+                { 0, 4, 8 }, { 2, 4, 6 }
+        };
 
-		for (int[] pattern : winPatterns) {
-			if (board.getCellMark(pattern[0]) == mark && board.getCellMark(pattern[1]) == mark
-					&& board.getCellMark(pattern[2]) == mark) {
-				return true;
-			}
-		}
-		return false;
-	}
-
+        for (int[] pattern : winPatterns) {
+            if (board.getCellMark(pattern[0]) == mark && board.getCellMark(pattern[1]) == mark
+                    && board.getCellMark(pattern[2]) == mark) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
